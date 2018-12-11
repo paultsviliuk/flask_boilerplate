b54391481afa3c9b871c7a36460128509ac685ea
angmar33 <paultsvilyuk@gmail.com>
2018-12-11 21:33:40 +0200

    security added

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ from flask_script import Manager
 
 from app import blueprint
 from app.main import create_app, db
-from app.main.model import user
+from app.main.model import user, blacklist
 
 app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
 app.register_blueprint(blueprint)
